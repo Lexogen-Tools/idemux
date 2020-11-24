d3be0f0212aae4d4c4a494e229741f238d012c8d
Falko Hofmann <falkohofmann@gmail.com>
2020-11-24 10:12:37 +0100

    bugfix: idemux now prints version correctly

--- idemux/__init__.py
+++ idemux/__init__.py
@@ -4,6 +4,7 @@ __author__ = """Falko Hofmann"""
 __email__ = 'falko.hofmann@lexogen.com'
 
 try:
-    from idemux._version import __version__
+    from idemux._version import version
+    __version__ = version
 except ImportError:
     __version__ = "not-installed"
